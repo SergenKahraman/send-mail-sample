379120408ef73b072f632991a640799e22dfe8f8
Sergen Kahraman <sergen_kahraman_@hotmail.com>
2022-12-01 01:19:26 +0300

    smtp eklendi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div
+    style="
+      display: flex;
+      flex-direction: column;
+      justify-items: center;
+      justify-content: center;
+    "
+    class="hello"
+  >
+    <h1>Mail Denemesi</h1>
+
+    <div style="display: flex; flex-direction: row; justify-content: center">
+      <div
+        style="
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          width: 30%;
+        "
+        class="form"
+      >
+        <label for="name">Adınız</label>
+        <input v-model="name" style="width: 100%" id="name" type="text" />
+        <label for="name">Mailiniz</label>
+        <input v-model="mail" style="width: 100%" id="mail" type="text" />
+        <label for="name">Mesajınız</label>
+        <textarea
+          v-model="content"
+          style="width: 100%"
+          id="content"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button @click="submit" style="margin-top: 15px" id="submit">
+          Gönder
+        </button>
+      </div>
+    </div>
   </div>
 </template>
-
+<script setup>
+import Email from "../helpers/smtp.js";
+</script>
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      name: null,
+      mail: null,
+      content: null,
+    };
+  },
+  methods: {
+    submit() {
+      //TODO: ücretsiz bir hesap aç ve devam et !!!!!!!!!!!!!!!!!!!!
+      Email.send({
+        Host: "in-v3.mailjet.com",
+        Username: "info@3amakinateknolojileri.com.tr",
+        Password: "3amakine123",
+        To: "sergen_kahraman_@hotmail.com",
+        From: "info@3amakinateknolojileri.com.tr",
+        Subject: "This is the subject",
+        Body: "And this is the body",
+      }).then((message) => alert(message));
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.form label {
+  margin-top: 20px;
+}
 h3 {
   margin: 40px 0 0;
 }
