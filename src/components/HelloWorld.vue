ec311a9c46a9eea4f38678d4e95d8f1a0b4a5fdd
Sergen Kahraman <sergen_kahraman_@hotmail.com>
2022-12-04 14:00:19 +0300

    email.js ile bu işi çözebildik

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,28 @@
 <template>
-  <div
-    style="
-      display: flex;
-      flex-direction: column;
-      justify-items: center;
-      justify-content: center;
-    "
-    class="hello"
-  >
-    <h1>Mail Denemesi</h1>
-
-    <div style="display: flex; flex-direction: row; justify-content: center">
-      <div
-        style="
-          display: flex;
-          flex-direction: column;
-          align-items: flex-start;
-          width: 30%;
-        "
-        class="form"
-      >
-        <label for="name">Adınız</label>
-        <input v-model="name" style="width: 100%" id="name" type="text" />
-        <label for="name">Mailiniz</label>
-        <input v-model="mail" style="width: 100%" id="mail" type="text" />
-        <label for="name">Mesajınız</label>
-        <textarea
-          v-model="content"
-          style="width: 100%"
-          id="content"
-          cols="30"
-          rows="10"
-        ></textarea>
-        <button @click="submit" style="margin-top: 15px" id="submit">
-          Gönder
-        </button>
-      </div>
-    </div>
-  </div>
+  <form ref="form" @submit.prevent="sendEmail">
+    <label>Name</label>
+    <input type="text" name="user_name">
+    <label>Email</label>
+    <input type="email" name="user_email">
+    <label>Message</label>
+    <textarea name="message"></textarea>
+    <input type="submit" value="Send">
+  </form>
 </template>
-<script setup>
-import Email from "../helpers/smtp.js";
-</script>
-<script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-  data() {
-    return {
-      name: null,
-      mail: null,
-      content: null,
-    };
-  },
-  methods: {
-    submit() {
-      //TODO: ücretsiz bir hesap aç ve devam et !!!!!!!!!!!!!!!!!!!!
-      Email.send({
-        Host: "in-v3.mailjet.com",
-        Username: "info@3amakinateknolojileri.com.tr",
-        Password: "3amakine123",
-        To: "sergen_kahraman_@hotmail.com",
-        From: "info@3amakinateknolojileri.com.tr",
-        Subject: "This is the subject",
-        Body: "And this is the body",
-      }).then((message) => alert(message));
-    },
-  },
-};
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.form label {
-  margin-top: 20px;
+<script>
+import emailjs from '@emailjs/browser';
+
+export default {
+  methods: {
+    sendEmail() {
+      emailjs.sendForm('service_a7gad19', 'template_t1f6s41', this.$refs.form, 'WBhil7IpZpstrBICw')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+        }, (error) => {
+            console.log('FAILED...', error.text);
+        });
+    }
+  }
 }
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+</script>
